28f236afdff86e8cc686ef518b2bf97971296ee0
Devilfloh <poetzschflorian@gmxde>
2024-06-10 16:12:53 +0200

    Implemented Login Window

---
+++ HumanResourcesApp/Login.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HumanResourcesApp
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Login(object sender, RoutedEventArgs e)
+        {
+            string username = textBoxUsername.Text;
+            string password = textBoxPassword.Password;
+
+            if (username == "test" && password == "test")
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                Debug.WriteLine("Wrong Username or Password!");
+            }
+        }
+    }
+}
---
+++ HumanResourcesApp/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResourcesApp
+{
+    public readonly struct DateFormat
+    {
+        public DateFormat(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public int Day { get; init; }
+        public int Month { get; init; }
+        public int Year { get; init; }
+    }
+
+    internal class Person
+    {
+        public string FirstName { get; private set; }
+        public string MiddleName { get; private set; }
+        public string LastName { get; private set; }
+        public string Gender { get; private set; }
+        public DateFormat Birthdate { get; private set; }
+        public string PostalCode { get; private set; }
+        public string StreetAdress { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public string Country { get; private set; }
+
+        public int Age
+        {
+            get
+            {
+                DateTime thisDay = DateTime.Today;
+
+                return thisDay.Year - Birthdate.Year;
+            }
+        }
+
+        public Person(string firstName, string middleName, string lastName, string gender, DateFormat birthdate, string streetAdress, string postalCode, string city, string state, string country)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+            Gender = gender;
+            Birthdate = birthdate;
+            StreetAdress = streetAdress;
+            PostalCode = postalCode;
+            City = city;
+            State = state;
+            Country = country;
+        }
+    }
+}
